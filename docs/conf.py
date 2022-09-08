8c8b9f729089126d75ce29152d8f6fd7c4118bc0
Tzach Livyatan <tzach@scylladb.com>
2022-09-08 20:51:49 +0300

    Update html_baseurl to iot

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ html_sidebars = {'**': ['side-nav.html']}
 htmlhelp_basename = 'ScyllaDocumentationdoc'
 
 # URL which points to the root of the HTML documentation. 
-html_baseurl = 'https://care-pet.docs.scylladb.com'
+html_baseurl = 'https://iot.docs.scylladb.com'
 
 # Dictionary of values to pass into the template engine’s context for all pages
 html_context = {'html_baseurl': html_baseurl}
